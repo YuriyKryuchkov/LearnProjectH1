ebd46033e138b2870115c4194c765493f84c4713
Yuriy Kryuchkov <ysilber175@gmail.com>
2022-11-13 16:02:12 +0300

    Add the second Hello World

--- src/main/java/com/setup/LearnProjectH1/MainApp.java
+++ src/main/java/com/setup/LearnProjectH1/MainApp.java
@@ -3,5 +3,6 @@ package com.setup.LearnProjectH1;
 public class MainApp {
     public static void main(String[] args)   {
         System.out.println("Hello World");
+        System.out.println("Hello World");
     }
 }
